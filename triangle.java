a98777cfc1f2635aad11118f7537ecfbcfea8104
Yuriy <uras@i.ua>
2016-03-09 16:05:05 +0200

    Create triangle.java

---
+++ triangle.java
@@ -0,0 +1,17 @@
+public class Triangle{
+  public static void main (String[] args){
+  
+  int baseSide=x;
+  int height=y;
+  int total=1/2*x*y;
+  
+  String triangleArea="Area";
+  
+  
+  
+  System.out.println(triangleArea);
+  
+  }
+  
+  
+}
