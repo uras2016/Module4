e4ba26cd32db8e4fbfb7891a1836fad94044bcc8
Yuriy <uras@i.ua>
2016-03-09 21:59:10 +0200

    Update triangle.java

--- triangle.java
+++ triangle.java
@@ -1,17 +1,14 @@
-public class Triangle{
-  public static void main (String[] args){
-  
-  int baseSide=x;
-  int height=y;
-  int total=1/2*x*y;
-  
-  String triangleArea="Area";
-  
-  
-  
-  System.out.println(triangleArea);
-  
-  }
-  
-  
+package goit;
+
+public class Triangle {
+
+public static void main(String[] args){
+    int baseSide = 5;
+    int height = 10;
+    double area;
+    area = 0.5*baseSide*height;
+
+    System.out.println("Triangle area is:" + area);
+
+}
 }
