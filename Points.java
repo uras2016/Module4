9fe927acaa6d5101d9fac261594dc02d0131ef89
Yuriy <uras@i.ua>
2016-03-10 09:07:08 +0200

    Create Points.java

---
+++ Points.java
@@ -0,0 +1,15 @@
+public class Points {
+ 
+    public static void main(String[] args) {
+        double x1;
+        double y1;
+        double x2;
+        double y2;
+        double z;
+    
+        z=Point2D.distance(x1,y1,x2,y2);
+        System.out.println("расстояние между двумя точками: "+z);
+    
+    
+ }
+}
