a1d9195c600a83ca65dd01a5181136aa632b15fe
Yuriy <uras@i.ua>
2016-03-10 12:34:25 +0200

    Update Points.java

--- Points.java
+++ Points.java
@@ -1,15 +1,10 @@
 public class Points {
  
     public static void main(String[] args) {
-        double x1;
-        double y1;
-        double x2;
-        double y2;
-        double z;
+        
+     double distance(double x1, double y1, double x2,double y2)
     
-        z=Point2D.distance(x1,y1,x2,y2);
-        System.out.println("расстояние между двумя точками: "+z);
+        System.out.println("расстояние между двумя точками: "+ distance);
     
-    
- }
+  }
 }
